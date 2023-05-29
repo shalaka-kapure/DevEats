d630a84f3731a81d35f5f7966de43e382c012120
Shalaka <111489373+shalaka-kapure@users.noreply.github.com>
2023-05-29 13:21:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HeaderComponent from "./components/HeaderComponent";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Error from "./components/Error";
+import Contact from "./components/Contact";
+import RestaurantMenu from "./components/RestaurantMenu";
+import Profile from "./components/Profile";
+import ShimmerUI from "./components/ShimmerUI";
+import Cart from "./components/Cart";
+import { Provider } from "react-redux";
+import store from "./utils/store";
 
-function App() {
+//Lazy loading
+const Instamart = lazy(() => import("./components/Instamart")); // this is a promise
+
+const AppLayout = () => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const toggleCart = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <HeaderComponent toggleCart={toggleCart} />
+      {isCartOpen && <Cart />}
+      <Routes>
+        <Route path="/" element={<Body />} />
+        <Route path="/about" element={<About />}>
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/restaurant/:id" element={<RestaurantMenu />} />
+        <Route path="/instamart" element={<Suspense fallback={<ShimmerUI />}><Instamart /></Suspense>} />
+      </Routes>
+      <Footer />
+    </Provider>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Router>
+      <AppLayout />
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,65 @@
+import { IMG_CDN_URL } from "../config";
+
+//1) using props const RestaurantCard = (props) => {
+//2) const RestaurantCard = ({restaurant}) => { destructuring props
+
+//3) const RestaurantCard = ({restaurant}) => { destructuring props and its children
+//const {name, cuisines,cloudinaryImageId, lastMileTravelString } = restaurant.data;
+
+//4)destructuring children on the fly and 5) using map
+const RestaurantCard = ({
+  cloudinaryImageId,
+  name,
+  avgRating,
+  deliveryTime,
+  cuisines,
+  slaString,
+  costForTwo,
+  aggregatedDiscountInfo,
+}) => {
+  return (
+    <div className="md:w-60 shadow-md md:shadow-none py-4 px-4 md:py-2  hover:shadow-2xl rounded flex flex-col gap-1 text-[0.7rem] text-[#535665] lg:h-[320px] h-[380px]">
+    <img
+      src={
+        IMG_CDN_URL +
+        (cloudinaryImageId === ""
+          ? "s6fhwzl0tss0vgrqvcid"
+          : cloudinaryImageId)
+      }
+      alt=""
+      className=" rounded object-cover"
+    />
+    <div className="res-details px-2">
+      <h4 className="font-medium text-base text-black">{name}</h4>
+      {/* 1) <h2>{props.restaurant.data?.name}</h2> */}
+      {/* 2) <h2>{restaurant.data?.name}</h2> */}
+      {/* 3), 4), 5) <h2>{name}}</h2> */}{" "}
+      {/* If props are further destructured*/}
+      <span className="h-[40px]">{cuisines.join(", ")}</span>
+        <div className="flex justify-between items-center my-2 font-medium">
+          <div className="flex items-center gap-1 px-1 text-white bg-green-500 font-semibold">
+            <span className="text-[0.6rem]">&#9733;</span>
+            <span className="text-[0.6rem]">
+              {avgRating === "--" ? "4.2" : avgRating}
+            </span>
+          </div>
+          <div className="w-[3px] h-[3px] rounded-full bg-black"></div>
+          <span className="">{slaString}</span>
+          <div className="res-price">
+            <span className="text-xs">
+             ₹{costForTwo / 100} FOR TWO
+            </span>
+          </div>
+        </div>
+        <div className="flex border-t pt-4 gap-2  font-semibold"></div>
+        <span className="text-[#a0522d] text-center">
+          {!aggregatedDiscountInfo?.shortDescriptionList[0]?.meta
+            ? "30% off | Use NEWFUD"
+            : aggregatedDiscountInfo?.shortDescriptionList[0]?.meta}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantCard;
